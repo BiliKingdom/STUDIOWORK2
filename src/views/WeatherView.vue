95922c976c51227f6f0a85057ee13409b0a63028
QINGTIAN LIU <103679162+BiliKingdom@users.noreply.github.com>
2025-10-09 17:40:28 +1100

    Updated WeatherView.vue

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -67,7 +67,7 @@ export default {
     },
     iconUrl() {
       return this.weatherData
-        ? `http://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+        ? `https://openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
         : null
     }
   },
@@ -80,7 +80,7 @@ export default {
         navigator.geolocation.getCurrentPosition(
           async (position) => {
             const { latitude, longitude } = position.coords
-            const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`
             await this.fetchWeatherData(url)
           },
           (error) => {
@@ -108,7 +108,7 @@ export default {
         return
       }
 
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`
       await this.fetchWeatherData(url)
     }
   }
